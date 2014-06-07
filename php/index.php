f3583e9d18bba5559b1e591b9ab6e6c275d3ffa0
daibutsuda <nakamura.masakatsu@gmail.com>
2014-06-08 06:28:38 +0900

    デバッグ用

--- php/index.php
+++ php/index.php
@@ -8,18 +8,15 @@
 	$reii = New Reii();
 	$kenkou = New Kenkou();
 	$seikaku = New Seikaku();
-
+	print_r($seimei->kanji);
+	
 	$seimei->kakusu('中村', '昌克');
 
-	echo $seimei->sei . ' ' . $seimei->mei . 'さんへのアドバイス<br />';
-	echo $seimei->kanji['中'] . "<br />";
-	echo $seimei->kanji['村'] . "<br />";
-	echo $seimei->kanji['昌'] . "<br />";
-	echo $seimei->kanji['克'] . "<br />";
-	echo '天画:' . $seimei->tenkaku . '<br />' . $reii->mongon[$seimei->tenkaku] . '<br />';
-	echo '人画:' . $seimei->jinkaku . '<br />' . $reii->mongon[$seimei->jinkaku] . '<br />';
-	echo '地画:' . $seimei->chikaku . '<br />' . $reii->mongon[$seimei->chikaku] . '<br />';
-	echo '総画:' . $seimei->soukaku . '<br />' . $reii->mongon[$seimei->soukaku] . '<br />';
-	echo '性格:' . $seimei->seikaku . '<br />' . $seikaku->mongon[$seimei->seikaku] . '<br />';
-	echo '健康:' . $seimei->kenkou . '<br />' . $kenkou->mongon[$seimei->kenkou] . '<br />';
+	echo $seimei->sei . ' ' . $seimei->mei . 'さんへのアドバイス<br />\n';
+	echo '天画:' . $seimei->tenkaku . '<br />' . $reii->mongon[$seimei->tenkaku] . '<br />\n';
+	echo '人画:' . $seimei->jinkaku . '<br />' . $reii->mongon[$seimei->jinkaku] . '<br />\n';
+	echo '地画:' . $seimei->chikaku . '<br />' . $reii->mongon[$seimei->chikaku] . '<br />\n';
+	echo '総画:' . $seimei->soukaku . '<br />' . $reii->mongon[$seimei->soukaku] . '<br />\n';
+	echo '性格:' . $seimei->seikaku . '<br />' . $seikaku->mongon[$seimei->seikaku] . '<br />\n';
+	echo '健康:' . $seimei->kenkou . '<br />' . $kenkou->mongon[$seimei->kenkou] . '<br />\n';
 ?>
