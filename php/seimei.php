ef3e872c8b92a8a374d59496eaaf4400ec2f8312
daibutsuda <nakamura.masakatsu@gmail.com>
2014-06-07 20:10:33 +0900

    バグ修正

--- php/seimei.php
+++ php/seimei.php
@@ -110,12 +110,12 @@ class Seimei {
 		}
 		
 		// 天画・人画・地画の下一桁の算出(10で割った余りを取るだけ)
-		$tenshimo = $this->tenkaku % 10;
-		$jinshimo = $this->jinkaku % 10;
-		$chishimo = $this->chikaku % 10;
+		$this->tenshimo = $this->tenkaku % 10;
+		$this->jinshimo = $this->jinkaku % 10;
+		$this->chishimo = $this->chikaku % 10;
 		
 		// 性格診断の準備
-		$this->seikaku = $jinshimo; 人画の下一桁で決まる
+		$this->seikaku = $this->jinshimo; 人画の下一桁で決まる
 
 		// 陰陽五行のシリアル番号の算出(詳しくはkenkou.phpを参照)
 		$this->kenkou = $this->f($this->tenshimo) * 25 + $this->f($this->jinshimo) * 5 + $this->f($this->chishimo);
