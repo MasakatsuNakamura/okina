a04121c42995d15ec7932d11ab1108ed626412c4
daibutsuda <nakamura.masakatsu@gmail.com>
2014-06-07 20:01:22 +0900

    バグ修正

--- php/index.php
+++ php/index.php
@@ -12,10 +12,10 @@
 	$seimei->kakusu('中村', '昌克');
 
 	echo $seimei->sei . ' ' . $seimei->mei . 'さんへのアドバイス<br />';
-	echo $seimei->tenkaku . '<br />' . $reii->mongon($seimei->tenkaku);
-	echo $seimei->jinkaku . '<br />' . $reii->mongon($seimei->jinkaku);
-	echo $seimei->chikaku . '<br />' . $reii->mongon($seimei->chikaku);
-	echo $seimei->soukaku . '<br />' . $reii->mongon($seimei->soukaku);
-	echo $seimei->seikaku . '<br />' . $seikaku->mongon($seimei->seikaku);
-	echo $seimei->kenkou . '<br />' . $kenkou->mongon($seimei->kenkou);
+	echo $seimei->tenkaku . '<br />' . $reii->mongon[$seimei->tenkaku];
+	echo $seimei->jinkaku . '<br />' . $reii->mongon[$seimei->jinkaku];
+	echo $seimei->chikaku . '<br />' . $reii->mongon[$seimei->chikaku];
+	echo $seimei->soukaku . '<br />' . $reii->mongon[$seimei->soukaku];
+	echo $seimei->seikaku . '<br />' . $seikaku->mongon[$seimei->seikaku];
+	echo $seimei->kenkou . '<br />' . $kenkou->mongon[$seimei->kenkou];
 ?>
