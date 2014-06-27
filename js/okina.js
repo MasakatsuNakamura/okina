986c5647c3a3f4c711bcb585dfa9e5fc2864e7c2
ma-nakamura <ma-nakamura@next.sanix.jp>
2014-06-27 17:12:10 +0900

    jQuery Mobile対応

---
+++ js/okina.js
@@ -0,0 +1,31 @@
+$(document).on("pagebeforeshow", "#kantei", function () {
+	var sei = $("#sei").val();
+	var mei = $("#mei").val();
+	var sex = $("#sex").val();
+	var marry = $("#marry").val();
+	$.ajax({
+		type: "POST",
+		url: "php/index.php",
+		data: 'sei=' + sei + '&mei=' + mei + '&sex=' + sex + '&marry=' + marry,
+		success: function (json) {
+			if (json) {
+				$("#kantei-content").html(json['content']);
+				$("#kantei-header").html(json['header']);
+			}
+		}
+	});
+});
+
+function kantei(sei, mei, sex, marry) {
+	$.ajax({
+		type: "POST",
+		url: "php/index.php",
+		data: 'sei=' + sei + '&mei=' + mei + '&sex=' + sex + '&marry=' + marry,
+		success: function (json) {
+			if (json) {
+				$("#kantei-content").html(json['content']);
+				$("#kantei-header").html(json['header']);
+			}
+		}
+	});
+}
