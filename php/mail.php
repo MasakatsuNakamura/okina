fbc59e5f993f02dcd7c0899dae50c0fbba13d00c
ma-nakamura <ma-nakamura@next.sanix.jp>
2014-09-27 17:29:28 +0900

    問い合わせ機能バグ修正

--- php/mail.php
+++ php/mail.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type: text/html; charset=utf-8;');
 require 'vendor/autoload.php';
 $sendgrid = new SendGrid(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
@@ -8,3 +9,5 @@ setFrom($_POST['email'])->
 setSubject('Query from seimei.asia')->
 setText($_POST['email'] . "\n" . $_POST['query']);
 $response = $sendgrid->send($message);
+?>
+<meta http-equiv="refresh" content="0;URL=/">
