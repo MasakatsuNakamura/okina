3f037274b38a2f4939ae34411e6c73a3146b9b3f
ma-nakamura <ma-nakamura@next.sanix.jp>
2014-10-21 16:32:35 +0900

    パス変更

--- fb-app.php
+++ fb-app.php
@@ -3,15 +3,15 @@ header('Content-type: text/html; charset=utf-8;');
 
 date_default_timezone_set('Asia/Tokyo');
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
-require '../php/seimei.php';
-require '../php/reii.php';
-require '../php/kenkou.php';
-require '../php/seikaku.php';
-require '../php/meimei.php';
-require '../php/kanji.php';
-require '../php/snipets.php';
+require 'php/seimei.php';
+require 'php/reii.php';
+require 'php/kenkou.php';
+require 'php/seikaku.php';
+require 'php/meimei.php';
+require 'php/kanji.php';
+require 'php/snipets.php';
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
@@ -66,10 +66,10 @@ if ($session) {
 	<title><?php echo $seimei->sei ?> <?php echo $seimei->mei ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-	<link rel="stylesheet" href="../css/default.css" />
+	<link rel="stylesheet" href="css/default.css" />
 	<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
-	<script type="text/javascript" src="../js/okina.js"></script>
+	<script type="text/javascript" src="js/okina.js"></script>
 	<?php googleAnalytics() ?>
 </head>
 
