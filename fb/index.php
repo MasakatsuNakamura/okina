42a971ebdf50eac7915422fa388f7c89c6cff818
ma-nakamura <ma-nakamura@next.sanix.jp>
2014-10-21 15:49:48 +0900

    プライバシーポリシーページ追加

--- fb/index.php
+++ fb/index.php
@@ -60,10 +60,10 @@ if ($session) {
 <title><?php echo $seimei->sei ?> <?php echo $seimei->mei ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-	<link rel="stylesheet" href="css/default.css" />
+	<link rel="stylesheet" href="../css/default.css" />
 	<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
-	<script type="text/javascript" src="js/okina.js"></script>
+	<script type="text/javascript" src="../js/okina.js"></script>
 	<?php googleAnalytics() ?>
 </head>
 
