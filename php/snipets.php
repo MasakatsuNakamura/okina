6a27fa708b47a9edfff26018237ee5d720acc78b
ma-nakamura <ma-nakamura@next.sanix.jp>
2014-10-22 08:54:16 +0900

    文言変更

--- php/snipets.php
+++ php/snipets.php
@@ -82,16 +82,12 @@ function seimeiHeader(Seimei $seimei) {
 function seimeiBody(Seimei $seimei) {
 ?>
 <body>
-	<h2><?php echo $seimei->sei ?>さんの命名・改名例</h2>
-	<hr />
-	<h3 style='color:blue'>男子（男性）の場合</h2>
-	<div><?php echo $seimei->meimei('M') ?></div>
-	<hr />
-	<h3 style='color:red'>女子（女性）の場合</h2>
-	<div><?php echo $seimei->meimei('F') ?></div>
+	<h2>あなた(<?php echo $seimei-sei . "さん (" . ($seimei->sei == 'M' ? '男性' : '女性'); ?>)の改名例</h2>
+	<a href="http://www.seimei.asia/" target="blank">結果が表示されない場合や、もっと占いたい場合は「あじあ姓名うらない（本家サイト）」までどうぞ！赤ちゃんの命名にも使えます</a>
+	<div><?php echo $seimei->meimei($seimei->sei) ?></div>
 	<hr />
 	
-	<h2><?php echo $seimei->sei . " " . $seimei->mei ?>さんの運勢 (総合得点：<?php echo $seimei->grand_score() ?>点)</h2>
+	<h2>あなた(<?php echo $seimei->sei . " " . $seimei->mei . "さん (" . ($seimei->sei == 'M' ? '男性' : '女性'); ?>の運勢 (総合得点：<?php echo $seimei->grand_score() ?>点)</h2>
 	<h3>人画 <?php echo $seimei->jinkaku . "画 (" . $seimei->score($seimei->jinkaku) . "点)" ?></h3>
 	<p style="color:blue;font-weight:bold;">基礎運。一生の運勢を司ります。結婚により姓が変わると基礎運も変化しますが、この場合中年以降に強く現れます。</p>
 	<p><?php echo $seimei->jinkaku . "画:" . $seimei->reii_description($seimei->jinkaku) . " (" . $seimei->score($seimei->jinkaku) . "点)" ?></p>
