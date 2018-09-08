0a629f4dfae51c4c23f06f72a469301ba65877b1
Masakatsu Nakamura <nakamura.masaskatsu@gmail.com>
2018-09-08 16:28:43 +0900

    SendGridログイン情報を平書き

--- mail-confirm.php
+++ mail-confirm.php
@@ -62,7 +62,7 @@ $authcode = rand(10000, 99999);
 
 </html>
 <?php
-$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
+$sendgrid = new SendGrid('app26677709@heroku.com', '6angkvnt9926');
 $message = new SendGrid\Email();
 $message->
 	addTo($_POST['email'])->
