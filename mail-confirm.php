4ebb04634201e90c9dc1babce71f3aa8da2748c4
Masakatsu Nkamaura <masakatsu.nakamura@gmail.com>
2018-09-16 11:59:17 +0900

    SendGridパスワードを環境変数に記述

--- mail-confirm.php
+++ mail-confirm.php
@@ -62,7 +62,7 @@ $authcode = rand(10000, 99999);
 
 </html>
 <?php
-$sendgrid = new SendGrid('app26677709@heroku.com', '6angkvnt9926');
+$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
 $message = new SendGrid\Email();
 $message->
 	addTo($_POST['email'])->
